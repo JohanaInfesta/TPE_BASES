63e57948da048784523a1ce8bc81a472a8a7bca6
Johana Infesta <johana.infesta@gmail.com>
2019-06-02 11:57:46 -0300

    carpetas vacias y vaciar js

--- config/ConfigApp.php
+++ config/ConfigApp.php
@@ -6,30 +6,7 @@ class ConfigApp
     public static $PARAMS = 'params';
     public static $ACTIONS = [
         ''=> 'IndexController#index',
-        //Configuración para url's items
-        'booksList' => 'BookController#index',
-        'book' => 'BookController#description',
-        'addBook' => 'BookController#create',
-        'editBook' => 'BookController#edit',
-        'saveBook' => 'BookController#store',
-        'deleteBook' => 'BookController#delete',
-        'deleteBookImage' => 'BookController#deleteImage',
-        //Configuración para url's autores
-        'allAuthors' => 'AuthorController#index',
-        'author' => 'AuthorController#booksByAuthor',
-        'addAuthor' => 'AuthorController#create',
-        'saveAuthor' => 'AuthorController#store',
-        'deleteAuthor' => 'AuthorController#delete',
-        'editAuthor' => 'AuthorController#edit',
-        //Configuración para url's login, logout y verificaciones
-        'login' => 'LoginController#index',
-        'registro' => 'LoginController#registro',
-        'logout' => 'LoginController#destroy',
-        'verifyUser' => 'LoginController#verify',
-        'addUser' => 'LoginController#create',
-        'deleteUser' => 'LoginController#delete',
-        'adminUsers' => 'LoginController#listaUsers',
-        'permisoSuperUser' => 'LoginController#superUser'
+      
     ];
 
 }
